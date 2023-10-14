c6350e85d60cb644c64f4fe522cdab36bbda2a46
Diana0422 <diana.pasquali@alumni.uniroma2.eu>
2023-10-14 02:59:58 +0200

    New experiment setting and added groovy script to jmeter test plan to recover qosClass and to add it to jmeter logs

--- utils/utility_analyze_2.py
+++ utils/utility_analyze_2.py
@@ -10,11 +10,12 @@ penalty = 0
 utility = 0
 
 for entry in os.listdir(DIR):
-    m = re.match("results-(\d+).csv", entry)
+    m = re.match("results_(\d+).csv", entry)
     if m is None:
         continue
     users = int(m.groups()[0])
     df = pd.read_csv(os.path.join(DIR, entry))
+    print(df)
 
     with open(os.path.join(DIR, f"utilityResults_{users}.csv"), "w") as uf:
         print("ClassName,Utility,Penalty,NetUtility", file=uf)
@@ -22,9 +23,17 @@ for entry in os.listdir(DIR):
         experiment_time = (df.timeStamp.max() - df.timeStamp.min()) / 1000.0
         completed = df[df.responseCode == 200]
         completed_count = completed.responseCode.count()
+        arrivalRate = df.responseCode.count()/experiment_time
+
+        # calculate mean elapsed
+        elapsed_mean = df["elapsed"].mean()/1000
+        print(f"elapsed mean (s): {elapsed_mean}")
+        print(f"arrival rate (r/s): {arrivalRate}")
+        print(f"num. users: {elapsed_mean/arrivalRate}")
+
+        """
         default = df[df.QoSClass == "default"]
         premium = df[df.QoSClass == "premium"]
-
         for request in df.rows:
             if request["QoSClass"] == "default":
                 if request["elapsed"] <= float('inf'):
@@ -36,6 +45,8 @@ for entry in os.listdir(DIR):
                     utility += 10
                 else:
                     penalty += 10
+
+        """
 
         net_utility = utility - penalty
 
